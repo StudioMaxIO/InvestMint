4fd4d955d49f61c2f9eda90bf66a02b81dc602eb
= <max@medicalkush.org>
2020-09-24 21:56:42 -0700

    reload on web3 account change

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0fbc3d31582d25a6ef52";
+/******/ 	var hotCurrentHash = "00de7c912abe90d3f186";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- ethereum/web3.js
+++ ethereum/web3.js
@@ -10,6 +10,9 @@ if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
   // in browser and metamask is running
   web3 = new Web3(window.ethereum);
   window.ethereum.enable();
+  window.ethereum.on("accountsChanged", function(accounts) {
+    window.location.reload(false);
+  });
 } else {
   // on the server or user is not running metamask
   const provider = new Web3.providers.HttpProvider(process.env.INFURA_API);
